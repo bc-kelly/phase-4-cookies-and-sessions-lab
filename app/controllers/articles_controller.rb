a034ddc9ed0d46530844bc4dd12e64a8dc7abf0d
Bridget Kelly <bckelly197@gmail.com>
2022-04-08 14:55:32 -0400

    commit tree 53f1bdfb37a44f9a4bbbce97f2a6899ffbb46f3c

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
@@ -7,8 +8,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+
+    if session[:page_views] <= 3
+      article = Article.find(params[:id])
+      render json:article
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
   end
 
   private
